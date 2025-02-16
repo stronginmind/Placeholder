a4856aace5630b89dc47a92094812673a6f80e15
Srypk <129874318+Srypk@users.noreply.github.com>
2025-02-16 19:15:49 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  let greeting: string = "Hello World!";
+</script>
+
+<svelte:head>
+  <title>
+    {greeting}
+  </title>
+  <meta name="description" content="Basic demo site for github pages and sveltekit.">
+</svelte:head>
+<main>
+  <div class="greeting">
+    <h1>{greeting}</h1>
+  </div>
+</main>
+
+<style>
+  main {
+    font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      "Open Sans",
+      "Helvetica Neue",
+      sans-serif;
+    height: 100%;
+  }
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,27 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: true,
+			strict: true
+		}),
+    paths: {
+			base: process.argv.includes('dev') ? '' : 'm1nxy.xyz'
+		},
+    prerender: {
+      handleHttpError: 'warn'
+    }
+	}
+};
+
+export default config;
