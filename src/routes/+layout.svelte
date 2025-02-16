3afc1f1d336506633caa0c75e5ce7d5f32c6cf5c
Srypk <129874318+Srypk@users.noreply.github.com>
2025-02-16 20:32:41 +0000

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
 </script>
 
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=caveat-brush:400" rel="stylesheet" />
+</svelte:head>
+
 <main>
   <slot />
 </main>
